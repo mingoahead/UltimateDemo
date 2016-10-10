c34ee621ca099c97f8c52d8d7dff001db1c6f061
niuming <mingfighter@gmail.com>
2016-10-10 10:27:33 +0800

    init

---
+++ UltimateDemo.pro
@@ -0,0 +1,119 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-10-10T09:34:58
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = UltimateDemo
+TEMPLATE = app
+
+## macros
+ItkRoot = $$PWD/../../3rd/InsightToolkit-4.8.0-minSizeRel
+VtkRoot = $$PWD/../../3rd/VTK-6.3.0-minSizeRel
+
+## itk-4.8.0
+win32: LIBS += -L$${ItkRoot}/lib/
+INCLUDEPATH += $${ItkRoot}/include/ITK-4.8/
+DEPENDPATH += $${ItkRoot}/include/ITK-4.8/
+
+## vtk-6.3.0
+win32: LIBS += -L$${VtkRoot}/lib/
+INCLUDEPATH += $${VtkRoot}/include/vtk-6.3/
+DEPENDPATH += $${VtkRoot}/include/vtk-6.3/
+
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    CTK-LIBS/LIBS/Core/ctkLogger.cpp \
+    CTK-LIBS/LIBS/Core/ctkUtils.cpp \
+    CTK-LIBS/LIBS/Core/ctkValueProxy.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKConnection.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKObjectEventsObserver.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKChartView.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKColorTransferFunction.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKScalarsToColorsView.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKScalarsToColorsWidget.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKThresholdWidget.cpp \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKVolumePropertyWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkBasePopupWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkCollapsibleGroupBox.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkColorDialog.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkColorPickerButton.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleRangeSlider.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleSlider.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleSpinBox.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkExpandButton.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkHistogram.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkMaterialPropertyPreviewLabel.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkMaterialPropertyWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkPopupWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkProxyStyle.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkRangeSlider.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkRangeWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkSliderWidget.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunction.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionBarsItem.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionControlPointsItem.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionGradientItem.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionItem.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionRepresentation.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionScene.cpp \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionView.cpp
+
+HEADERS  += mainwindow.h \
+    Utils/itklibs.h \
+    Utils/vtklibs.h \
+    Utils/itkutils.h \
+    CTK-LIBS/LIBS/Core/ctkCoreExport.h \
+    CTK-LIBS/LIBS/Core/ctkLogger.h \
+    CTK-LIBS/LIBS/Core/ctkPimpl.h \
+    CTK-LIBS/LIBS/Core/ctkSingleton.h \
+    CTK-LIBS/LIBS/Core/ctkUtils.h \
+    CTK-LIBS/LIBS/Core/ctkValueProxy.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVisualizationVTKCoreExport.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKConnection.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKConnection_p.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKObject.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Core/ctkVTKObjectEventsObserver.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVisualizationVTKWidgetsExport.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKChartView.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKColorTransferFunction.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKScalarsToColorsView.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKScalarsToColorsWidget.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKThresholdWidget.h \
+    CTK-LIBS/LIBS/Visualization/VTK/Widgets/ctkVTKVolumePropertyWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkBasePopupWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkBasePopupWidget_p.h \
+    CTK-LIBS/LIBS/Widgets/ctkCollapsibleGroupBox.h \
+    CTK-LIBS/LIBS/Widgets/ctkColorDialog.h \
+    CTK-LIBS/LIBS/Widgets/ctkColorPickerButton.h \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleRangeSlider.h \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleSlider.h \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleSpinBox.h \
+    CTK-LIBS/LIBS/Widgets/ctkDoubleSpinBox_p.h \
+    CTK-LIBS/LIBS/Widgets/ctkExpandButton.h \
+    CTK-LIBS/LIBS/Widgets/ctkHistogram.h \
+    CTK-LIBS/LIBS/Widgets/ctkMaterialPropertyPreviewLabel.h \
+    CTK-LIBS/LIBS/Widgets/ctkMaterialPropertyWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkPopupWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkPopupWidget_p.h \
+    CTK-LIBS/LIBS/Widgets/ctkProxyStyle.h \
+    CTK-LIBS/LIBS/Widgets/ctkRangeSlider.h \
+    CTK-LIBS/LIBS/Widgets/ctkRangeWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkSliderWidget.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunction.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionBarsItem.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionControlPointsItem.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionGradientItem.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionItem.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionRepresentation.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionScene.h \
+    CTK-LIBS/LIBS/Widgets/ctkTransferFunctionView.h \
+    CTK-LIBS/LIBS/Widgets/ctkWidgetsExport.h
+
+FORMS    += mainwindow.ui
---
+++ Utils/itkutils.h
@@ -0,0 +1,284 @@
+#ifndef IYKUTILS
+#define IYKUTILS
+
+# include "itkVersion.h"
+# include "itkImageIOFactory.h"
+# include "itkPNGImageIOFactory.h"
+# include "itkMetaImageIOFactory.h"
+# include "itkBMPImageIOFactory.h"
+# include "itkJPEGImageIOFactory.h"
+# include "itkGDCMImageIOFactory.h"
+# include "itkImageSeriesReader.h"
+# include "itkGDCMImageIO.h"
+# include "itkGDCMSeriesFileNames.h"
+# include "itkImageFileWriter.h"
+# include "itkMetaImageIO.h"
+# include "itkImage.h"
+# include "itkImageToVTKImageFilter.h"
+# include "itkImageSeriesWriter.h"
+# include "gdcmUIDGenerator.h"
+# include "itkNumericSeriesFileNames.h"
+# include "vtkSmartPointer.h"
+# include "vtkImageFlip.h"
+# include "vtkImageData.h"
+# include <string>
+# include <sstream>
+
+class ItkUtils
+{
+public:
+    static void RegisterAllImageIOFactories()
+    {
+        itk::PNGImageIOFactory::RegisterOneFactory();
+        itk::BMPImageIOFactory::RegisterOneFactory();
+        itk::MetaImageIOFactory::RegisterOneFactory();
+        itk::JPEGImageIOFactory::RegisterOneFactory();
+        itk::GDCMImageIOFactory::RegisterOneFactory();
+    }
+
+    ///
+    /// srcFolder: DICOM folder
+    /// destPath : .mhd file path
+    ///
+    template<typename InputElementType, typename OutputElementType>
+    static void DicomSeries2Raw(std::string srcFolder, std::string destPath)
+    {
+        const size_t Dimension = 3;
+        typedef itk::Image<InputElementType, Dimension> InputImageType;
+        typedef itk::Image<OutputElementType, Dimension> OutputImageType;
+
+        typedef itk::ImageSeriesReader<InputImageType> ReaderType;
+        typedef itk::GDCMImageIO ImageIOType;
+
+        ReaderType::Pointer reader = itk::ImageSeriesReader<InputImageType>::New();
+        ImageIOType::Pointer dicomIO = itk::GDCMImageIO::New();
+
+        reader->SetImageIO(dicomIO);
+
+        typedef itk::GDCMSeriesFileNames NamesGeneratorType;
+
+        NamesGeneratorType::Pointer nameGenerator = itk::GDCMSeriesFileNames::New();
+        nameGenerator->SetUseSeriesDetails(true);
+        nameGenerator->AddSeriesRestriction("0008|0021");
+
+        nameGenerator->SetDirectory(srcFolder);
+
+        try {
+            typedef std::vector< std::string >    SeriesIdContainer;
+
+            const SeriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
+            std::string seriesIdentifier = *seriesUID.begin();
+            std::vector< std::string > fileNames = nameGenerator->GetFileNames( seriesIdentifier );
+            reader->SetFileNames( fileNames );
+            reader->Update();
+        } catch (itk::ExceptionObject &ex) {
+            std::cout << ex.GetDescription() << std::endl;
+            throw;
+        }
+
+        try {
+            typedef itk::ImageFileWriter< OutputImageType > WriterType;
+
+            WriterType::Pointer writer = itk::ImageFileWriter<OutputImageType>::New();
+            writer->SetFileName(destPath);
+            writer->SetInput(reader->GetOutput());
+            writer->SetImageIO(itk::MetaImageIO::New());
+            writer->Update();
+        } catch (itk::ExceptionObject &ex) {
+            std::cerr << ex.GetDescription() << std::endl;
+            throw;
+        }
+    }
+
+    template<typename InputElementType, typename OutputElementType>
+    static void SaveAsStdDicom(std::string srcFolder, std::string destFolder)
+    {
+        const size_t InputDimension = 3;
+        const size_t OutputDimension = 2;
+        typedef itk::Image<InputElementType, InputDimension> InputImageType;
+        typedef itk::Image<OutputElementType, OutputDimension> OutputImageType;
+
+        typedef itk::ImageSeriesReader<InputImageType> ReaderType;
+        typedef itk::GDCMImageIO ImageIOType;
+
+        ReaderType::Pointer reader = itk::ImageSeriesReader<InputImageType>::New();
+        ImageIOType::Pointer dicomIO = itk::GDCMImageIO::New();
+
+        reader->SetImageIO(dicomIO);
+
+        typedef itk::GDCMSeriesFileNames NamesGeneratorType;
+
+        NamesGeneratorType::Pointer nameGenerator = itk::GDCMSeriesFileNames::New();
+        nameGenerator->SetUseSeriesDetails(true);
+        nameGenerator->AddSeriesRestriction("0008|0021");
+
+        nameGenerator->SetDirectory(srcFolder);
+
+        try {
+            typedef std::vector< std::string >    SeriesIdContainer;
+
+            const SeriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
+            std::string seriesIdentifier = *seriesUID.begin();
+            std::vector< std::string > fileNames = nameGenerator->GetFileNames( seriesIdentifier );
+            reader->SetFileNames( fileNames );
+            reader->Update();
+        } catch (itk::ExceptionObject &ex) {
+            std::cout << ex.GetDescription() << std::endl;
+            throw;
+        }
+
+        const InputImageType::SpacingType& inputSpacing = reader->GetOutput()->GetSpacing();
+        const InputImageType::RegionType& inputRegion = reader->GetOutput()->GetLargestPossibleRegion();
+        const InputImageType::SizeType& inputSize = inputRegion.GetSize();
+
+        typedef itk::NumericSeriesFileNames OutputNamesGeneratorType;
+
+        OutputNamesGeneratorType::Pointer outputNames = OutputNamesGeneratorType::New();
+        std::string seriesFormat(destFolder);
+        seriesFormat = seriesFormat + "/" + "IM%05d.dcm";
+        outputNames->SetSeriesFormat (seriesFormat);
+        outputNames->SetStartIndex (1);
+        outputNames->SetEndIndex (inputSize[2]);
+
+
+        ReaderType::DictionaryArrayType outputArray;
+
+        ReaderType::DictionaryRawPointer inputDict = (*(reader->GetMetaDataDictionaryArray()))[0];
+
+        gdcm::UIDGenerator suid;
+        std::string seriesUID = suid.Generate();
+        gdcm::UIDGenerator fuid;
+        std::string frameOfReferenceUID = fuid.Generate();
+
+        std::string studyUID;
+        std::string sopClassUID;
+        itk::ExposeMetaData<std::string>(*inputDict, "0020|000d", studyUID);
+        itk::ExposeMetaData<std::string>(*inputDict, "0008|0016", sopClassUID);
+        dicomIO->KeepOriginalUIDOn();
+        for (unsigned int f = 0; f < inputSize[2]; f++) {
+            ReaderType::DictionaryRawPointer dict = new ReaderType::DictionaryType;
+            CopyDictionary (*inputDict, *dict);
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|000d", studyUID);
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|000e", seriesUID);
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|0052", frameOfReferenceUID);
+            gdcm::UIDGenerator sopuid;
+            std::string sopInstanceUID = sopuid.Generate();
+            itk::EncapsulateMetaData<std::string>(*dict,"0008|0018", sopInstanceUID);
+            itk::EncapsulateMetaData<std::string>(*dict,"0002|0003", sopInstanceUID);
+            std::ostringstream value;
+            value.str("");
+            value << f + 1;
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|0013", value.str());
+
+            std::string oldSeriesDesc;
+            itk::ExposeMetaData<std::string>(*inputDict, "0008|103e", oldSeriesDesc);
+            value.str("");
+            value << oldSeriesDesc
+                  << inputSpacing[0] << ", "
+                  << inputSpacing[1] << ", "
+                  << inputSpacing[2];
+            unsigned lengthDesc = value.str().length();
+
+            std::string seriesDesc( value.str(), 0, lengthDesc > 64 ? 64 : lengthDesc);
+            itk::EncapsulateMetaData<std::string>(*dict,"0008|103e", seriesDesc);
+
+            // Series Number
+            value.str("");
+            value << 1001;
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|0011", value.str());
+
+            value.str("");
+            value << "surfaceRenderer : " << ITK_SOURCE_VERSION;
+
+            lengthDesc = value.str().length();
+            std::string derivationDesc( value.str(), 0, lengthDesc > 1024 ? 1024 : lengthDesc);
+            itk::EncapsulateMetaData<std::string>(*dict,"0008|2111", derivationDesc);
+
+            InputImageType::PointType position;
+            InputImageType::IndexType index;
+            index[0] = 0;
+            index[1] = 0;
+            index[2] = f;
+            reader->GetOutput()->TransformIndexToPhysicalPoint(index, position);
+
+            value.str("");
+            value << position[0] << "\\" << position[1] << "\\" << position[2];
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|0032", value.str());
+            value.str("");
+            value << position[2];
+            itk::EncapsulateMetaData<std::string>(*dict,"0020|1041", value.str());
+
+            outputArray.push_back(dict);
+        }
+
+        typedef itk::ImageSeriesWriter< InputImageType, OutputImageType >  WriterType;
+        WriterType::Pointer writer = WriterType::New();
+        writer->SetInput( reader->GetOutput() );
+        dicomIO->SetByteOrderToLittleEndian();
+        writer->SetImageIO( dicomIO );
+        writer->SetFileNames( outputNames->GetFileNames() );
+        writer->SetMetaDataDictionaryArray( &outputArray );
+
+        try {
+            writer->Update();
+        } catch( itk::ExceptionObject & excp ) {
+            std::cerr << excp << std::endl;
+            throw;
+        }
+    }
+
+    template<typename ItkImageType>
+    static vtkImageData* ToVtkImageData(ItkImageType* itkImageData)
+    {
+        typedef itk::ImageToVTKImageFilter<ItkImageType> Itk2VtkFilterType;
+
+        Itk2VtkFilterType::Pointer filter = Itk2VtkFilterType::New();
+        filter->SetInput(itkImageData);
+        filter->Update();
+
+        vtkSmartPointer<vtkImageFlip> flipX = vtkSmartPointer<vtkImageFlip>::New();
+        vtkSmartPointer<vtkImageFlip> flipY = vtkSmartPointer<vtkImageFlip>::New();
+
+        flipX->SetInputData(filter->GetOutput());
+
+        flipX->SetFilteredAxis(1);
+
+        flipX->Update();
+
+        flipY->SetInputData(flipX->GetOutput());
+
+        flipY->SetFilteredAxis(2);
+
+        flipY->Update();
+
+        vtkImageData* vtkImage = flipY->GetOutput();
+
+        return vtkImage;
+    }
+private:
+    static void CopyDictionary (itk::MetaDataDictionary &fromDict, itk::MetaDataDictionary &toDict)
+    {
+        typedef itk::MetaDataDictionary DictionaryType;
+
+        DictionaryType::ConstIterator itr = fromDict.Begin();
+        DictionaryType::ConstIterator end = fromDict.End();
+        typedef itk::MetaDataObject< std::string > MetaDataStringType;
+
+        while( itr != end ) {
+            itk::MetaDataObjectBase::Pointer  entry = itr->second;
+
+            MetaDataStringType::Pointer entryvalue =
+                    dynamic_cast<MetaDataStringType *>( entry.GetPointer() ) ;
+            if( entryvalue ) {
+                std::string tagkey   = itr->first;
+                std::string tagvalue = entryvalue->GetMetaDataObjectValue();
+                itk::EncapsulateMetaData<std::string>(toDict, tagkey, tagvalue);
+            }
+            ++itr;
+        }
+    }
+
+
+};
+#endif // IYKUTILS
+
